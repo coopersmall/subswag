b3e50bd5d5dfdd11eb3225879d27aec12faf7ec0
coopersmall <cooper.small@gladly.com>
2025-03-15 21:03:11 -0600

    refactor

--- .github/actions/verify-checklist/index.js
+++ .github/actions/verify-checklist/index.js
@@ -35,36 +35,38 @@ async function run({context, octokit}, issueNumber) {
               text: 'Please ensure that the PR includes the Required Acknowledgements checklist.'
           }
       });
-  } else {
-    const uncheckedBoxes = (checklistComment.body.match(/\[ \]/g) || []).length;
-    if (uncheckedBoxes > 0) {
-        await octokit.rest.checks.update({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            check_run_id: check.data.id,
-            status: 'completed',
-            conclusion: 'failure',
-            output: {
-                title: 'Missing Checklist Items',
-                summary: `Found ${uncheckedBoxes} unchecked item${uncheckedBoxes > 1 ? 's' : ''} in the PR checklist`,
-                text: 'Please complete all items in the Required Acknowledgements checklist before proceeding.'
-            }
-        });
-    } else {
-        await octokit.rest.checks.update({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            check_run_id: check.data.id,
-            status: 'completed',
-            conclusion: 'success',
-            output: {
-                title: 'Checklist Complete',
-                summary: 'All required checklist items have been completed.',
-                text: 'The PR checklist has been properly filled out.'
-            }
-        });
-    }
-  }
+    return;
+  } 
+
+  const uncheckedBoxes = (checklistComment.body.match(/\[ \]/g) || []).length;
+  if (uncheckedBoxes > 0) {
+      await octokit.rest.checks.update({
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          check_run_id: check.data.id,
+          status: 'completed',
+          conclusion: 'failure',
+          output: {
+              title: 'Missing Checklist Items',
+              summary: `Found ${uncheckedBoxes} unchecked item${uncheckedBoxes > 1 ? 's' : ''} in the PR checklist`,
+              text: 'Please complete all items in the Required Acknowledgements checklist before proceeding.'
+          }
+      });
+    return;
+  } 
+
+  await octokit.rest.checks.update({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      check_run_id: check.data.id,
+      status: 'completed',
+      conclusion: 'success',
+      output: {
+          title: 'Checklist Complete',
+          summary: 'All required checklist items have been completed.',
+          text: 'The PR checklist has been properly filled out.'
+      }
+  });
 }
 
 
